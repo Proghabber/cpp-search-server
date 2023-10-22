69cc92c62adfad62bb447d84924ed9b6de0958d2
Alex <rozenrol@mail.ru>
2023-10-22 17:03:52 +0800

    My first commit!

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,3 +2,19 @@
 // Напишите ответ здесь:
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+// Закомитьте изменения и отправьте их в свой репозиторий.
+#include <iostream>
+
+using namespace std;
+int main() {
+
+	int answer = 0;
+	for (int i = 0; i <= 100; i++) if (i == 3 || i / 10 == 3 || i % 10 == 3) {
+		cout << i << endl;
+		answer++;
+
+	};
+	cout << "From 0 to 100 number 3 have " << answer << " numbers";
+	return 0;
+
+}
